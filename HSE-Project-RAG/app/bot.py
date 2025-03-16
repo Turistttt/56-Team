ca9a352c8fd987c19cdb4b3d04712db466ed5361
Burov Dmitriy <ga232321@gmail.com>
2025-03-16 22:13:24 +0300

    checkpoint 5 refactoring

--- HSE-Project-RAG/app/bot.py
+++ HSE-Project-RAG/app/bot.py
@@ -1,27 +1,47 @@
-
-from RagClass import RagProject
 import telebot
-
-from extra import TOKEN
 import numpy as np
+import httpx
+from dotenv import load_dotenv
 
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+FASTAPI_URL = os.getenv('FASTAPI_URL')
 
 bot = telebot.TeleBot(TOKEN)
 
-
-rag_system = RagProject("meta-llama/Llama-3.2-3B-Instruct",
-                        'sentence-transformers/all-MiniLM-L6-v2')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Hello, how can I help you?")
 
 
-
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.send_message(message.chat.id, np.random.choice(["Thinking ...","Looking for an answer..."]))
-    bot.reply_to(message, rag_system.query(message.text,2)[0]['generated_text'])
+    # Переходим от асинхронного кода к синхронному для `httpx`
+    bot.send_message(message.chat.id, np.random.choice(["Thinking ...", "Looking for an answer..."]))
+
+    # Используем синхронный клиент
+    try:
+        payload = {
+            "message": message.text,
+            "num_results": 2
+        }
+
+        # Делаем обычный HTTP-запрос без async/await
+        with httpx.Client() as client:
+            response = client.post(FASTAPI_URL, json=payload,timeout = 120  )
+            response.raise_for_status()
+            data = response.json()
+            generated_text = data.get('generated_text', 'No response received')
+
+            bot.reply_to(message, generated_text)
+
+    except httpx.HTTPError as exc:
+        bot.send_message(message.chat.id, f"An error occurred: {exc}")
+
 
 if __name__ == '__main__':
     print("Bot starting...")
